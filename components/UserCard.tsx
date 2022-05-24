e9ac2b6bb9e143f81c56e7715edec42307336e56
William Gitta <73100027+KabakaWilliam@users.noreply.github.com>
2022-05-24 22:39:15 +0300

    changed card

--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -3,7 +3,7 @@ import LanguageBubble from "./LanguageBubble";
 
 const UserCard = () => {
   return (
-    <div className="w-[300px] h-[400px] snap-y overflow-y-auto snap-mandatory md:w-[400px] md:h-[500px] bg-[#222324]  cursor-pointer shadow-[#131414]  shadow-lg rounded-xl">
+    <div className="w-[300px] h-[450px] snap-y overflow-y-auto snap-mandatory md:w-[400px] md:h-[500px] bg-[#222324]  cursor-pointer shadow-[#131414]  shadow-lg rounded-xl">
       <div className="snap-start w-[100%] h-[100%] -green-500 rounded-t-xl  ">
         <div className="h-[80%] w-[100%]  "></div>
         <div className="h-[20%] w-[100%]  flex flex-wrap items-center justify-start gap-x-3 pl-2">
